9a07ea6f09f437da4bcfc3413c2f72f807003a91
Jeremias Quinteros <qjeremias263@gmail.com>
2025-09-25 17:02:02 -0300

    Creación de vistas/ componentes - uso de rutas dinámicas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { About } from "./pages/about/about";
+import { Home } from "./pages/home/home";
+import { Contact } from './pages/contact/contact';
+import { Products } from './pages/products/products';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, About, Home, Contact, Products],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
