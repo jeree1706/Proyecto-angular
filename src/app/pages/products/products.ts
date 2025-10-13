67f7668ca958369e16cc988e83eaf97b9c03049b
Jeremias Quinteros <qjeremias263@gmail.com>
2025-10-13 17:07:50 -0300

    home casi terminado

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -18,15 +18,15 @@ constructor (){
   this.infoTarjeta = [
   {
     id: "1t",
-    img: "../../../assets/pexels-pixabay-325876.jpg",
+    img: "../../../assets/pant.nike.webp",
     alt: "Foto de camisas",
-    titulo: "Camisas",
-    descripcion: "Foto tomada en una tienda de ropa",
+    titulo: "Pantalon urbano Nike",
+    descripcion: "Talles disp. : 38-43",
     anioPublicacion: 0
   },
    {
     id: "2t",
-    img: "../../../assets/Nike-Symbol.png",
+    img: "../../../assets/adidaspant.webp",
     alt: "Foto del logo de nike",
     titulo: "Nike",
     descripcion: "Logo de nike",
@@ -34,7 +34,7 @@ constructor (){
   },
    {
     id: "3t",
-    img: "../../../assets/adidaslogo.jpg",
+    img: "../../../assets/zapalacoste.webp",
     alt: "Foto del logo de adidas",
     titulo: "Adidas",
     descripcion: "Logo de adidas",
@@ -42,20 +42,20 @@ constructor (){
   },
    {
     id: "4t",
-    img: "../../../assets/Emblème-Gucci.jpg",
+    img: "../../../assets/remgucci.webp",
     alt: "Foto del logo de gucci",
     titulo: "Gucci",
     descripcion: "Logo de gucci",
     anioPublicacion: 0
   },
-  {
+  /*{
     id: "5t",
     img: "../../../assets/Lacoste-Simbolo.jpg",
     alt: "Foto del logo de lacoste",
     titulo: "Lacoste",
     descripcion: "Logo de Lacoste",
     anioPublicacion: 0
-  }
+  }*/
   ]
 }
 }
